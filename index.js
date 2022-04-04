65d4c59b120174370c03a49842725f008356816d
DESKTOP-BUMAKAD\82104 <thtto77@gmail.com>
2022-04-04 20:36:21 +0900

    정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://sypark:ww1120ww@bolierplate.k4fzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoDB Connected....'))
 .catch(err => console.log(err))
@@ -12,6 +22,22 @@ mongoose.connect('mongodb+srv://sypark:ww1120ww@bolierplate.k4fzn.mongodb.net/my
 
 app.get('/', (req, res) => {
   res.send('Hello World! ~ 안녕하세요')
+})
+
+
+app.post('/register', (req,res) => {
+    //회원가입할때 필요한 정보들을 클라이언트에서 가져오면 
+    //그것들을 데이터베이스에 넣어준다.
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
